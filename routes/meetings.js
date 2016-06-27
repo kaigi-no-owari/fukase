01b642e78045d001fc93283b1b28869b8c7a798f
oohira <oohira.dev@gmail.com>
2016-06-27 22:52:28 +0900

    スケジュール取得APIの実装 #7
    * GET /api/meetings
      * すべてのスケジュールを開始時刻順に取得
    * GET /api/meetings/{room_id}
      * 指定した会議室のスケジュールを開始時刻順に取得

--- routes/meetings.js
+++ routes/meetings.js
@@ -3,6 +3,19 @@ const router = express.Router(); // eslint-disable-line new-cap
 const pgp = require('pg-promise')();
 const db = pgp(process.env.DATABASE_URL);
 
+router.get('/', (req, res, next) => {
+  db.any('SELECT * FROM meetings ORDER BY start_at')
+  .then((data) => res.status(200).json(data))
+  .catch((err) => next(err));
+});
+
+router.get('/:room_id', (req, res, next) => {
+  db.any('SELECT * FROM meetings WHERE room_id = $1 ORDER BY start_at',
+    [req.params.room_id])
+  .then((data) => res.status(200).json(data))
+  .catch((err) => next(err));
+});
+
 function getCreatedMeetings(req) {
   if (!Array.isArray(req.body)) {
     return [];
