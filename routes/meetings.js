798b4ea2869306b014d4f3a2da7b14950bb2907c
oohira <oohira.dev@gmail.com>
2016-06-28 23:53:52 +0900

    登録処理のリファクタリング

--- routes/meetings.js
+++ routes/meetings.js
@@ -3,29 +3,36 @@ const router = express.Router(); // eslint-disable-line new-cap
 const pgp = require('pg-promise')();
 const db = pgp(process.env.DATABASE_URL);
 
+function getCreatedMeetings(req) {
+  if (!Array.isArray(req.body)) {
+    return [];
+  }
+  const now = new Date();
+  return req.body.map(m => ({
+    room_id: m.room_id,
+    title: m.title || '',
+    description: m.description || '',
+    start_at: new Date(m.start_at),
+    end_at: new Date(m.end_at),
+    registered_at: now,
+  }));
+}
+
 router.post('/', (req, res, next) => {
   // 単純に全データ削除してから登録し直す
   db.tx(t => {
     const queries = [];
     queries.push(t.none('DELETE FROM meetings'));
-    if (Array.isArray(req.body)) {
-      const now = new Date();
-      req.body.forEach(m => {
-        queries.push(t.none('INSERT INTO ' +
-          'meetings (id, room_id, title, description, start_at, end_at, registered_at)' +
-          'values (DEFAULT, $1, $2, $3, $4, $5, $6)',
-          [m.room_id, m.title, m.description, m.start_at, m.end_at, now]
-        ));
-      });
-    }
+    getCreatedMeetings(req).forEach(meeting => {
+      queries.push(t.none('INSERT INTO meetings' +
+        ' (id, room_id, title, description, start_at, end_at, registered_at)' +
+        ' values (DEFAULT, $(room_id), $(title), $(description), $(start_at),' +
+        ' $(end_at), $(registered_at))', meeting));
+    });
     return t.batch(queries);
   })
-  .then(() => {
-    res.status(201).json({ status: 201, message: 'Created' });
-  })
-  .catch(err => {
-    next(err);
-  });
+  .then(() => res.status(201).json({ status: 201, message: 'Created' }))
+  .catch(err => next(err));
 });
 
 module.exports = router;
